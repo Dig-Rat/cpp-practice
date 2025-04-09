f04c4b86bb6de4ee6b70d7907e52aa7d115c2675
Dig Rat <CyberianDigRat@gmail.com>
2025-04-08 23:09:35 -0500

    first commit

---
+++ Template/src/MyClass.cpp
@@ -0,0 +1,6 @@
+#include "MyClass.h"
+#include <iostream>
+
+void MyClass::sayHello() {
+    std::cout << "Hello from MyClass!" << std::endl;
+}
---
+++ Template/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "MyClass.h"
+
+int main() {
+    std::cout << "Hello from main!" << std::endl;
+
+    MyClass obj;
+    obj.sayHello();
+
+    return 0;
+}
